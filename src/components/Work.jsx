32003faf3dc9bfe6df90ff1db7cf4f2c64c740c2
Sarah Theoulle <sarah.theoulle@etu.umontpellier.fr>
2024-10-13 20:56:32 +0200

    add works

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,43 +1,39 @@
 import ProjectCard from './ProjectCard';
 
 const works = [
-    {
-      imgSrc: '/images/project-1.jpg',
-      title: 'Full stack music app',
-      tags: ['API', 'MVC', 'Development'],
-      projectLink: 'https://musify-5al0.onrender.com/'
-    },
-    {
-      imgSrc: '/images/project-2.jpg',
-      title: 'Free stock photo app',
-      tags: ['API', 'SPA'],
-      projectLink: 'https://pixstock-official.vercel.app/'
-    },
-    {
-      imgSrc: '/images/project-3.jpg',
-      title: 'Recipe app',
-      tags: ['Development', 'API'],
-      projectLink: ''
-    },
-    {
-      imgSrc: '/images/project-4.jpg',
-      title: 'Real state website',
-      tags: ['Web-design', 'Development'],
-      projectLink: 'https://github.com/codewithsadee-org/wealthome'
-    },
-    {
-      imgSrc: '/images/project-5.jpg',
-      title: 'eCommerce website',
-      tags: ['eCommerce', 'Development'],
-      projectLink: 'https://github.com/codewithsadee/anon-ecommerce-website'
-    },
-    {
-      imgSrc: '/images/project-6.jpg',
-      title: 'vCard Personal portfolio',
-      tags: ['Web-design', 'Development'],
-      projectLink: 'https://github.com/codewithsadee/vcard-personal-portfolio'
-    },
-  ];
+  {
+    imgSrc: '/images/seal-ci.svg', // Replace with relevant image path
+    title: 'SealCI',
+    tags: ['CI', 'Rust', 'Development'],
+    projectLink: 'https://github.com/dev-sys-do/sealci'
+  },
+  {
+    imgSrc: '/images/beep.svg', // Replace with relevant image path
+    title: 'Beep - Discord Clone',
+    tags: ['React', 'TailwindCSS', 'AdonisJS'],
+    projectLink: 'https://beep.ovh'
+  },
+  
+  {
+    imgSrc: '/images/twitch.svg', // Replace with relevant image path
+    title: 'Twitch Data Visualization',
+    tags: ['Python', 'Gephi', 'Data'],
+    projectLink: 'https://github.com/stheoulle/twitch-data-visualization'
+  },
+  {
+    imgSrc: '/images/polycloud.svg', // Replace with relevant image path
+    title: 'Polycloud Conference',
+    tags: ['Cloud', 'Conference', 'DevOps'],
+    projectLink: 'https://polycloud.fr/'
+  },
+  {
+    imgSrc: '/images/kdna.svg', // Replace with relevant image path
+    title: 'KDNA',
+    tags: ['Python', 'CLI', 'Backups'],
+    projectLink: 'https://github.com/somoslosdorados/KDNA'
+  }
+];
+
 
 const Work = () => {
     return (
