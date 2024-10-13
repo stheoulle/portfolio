383d4486413bfe81e2b9fdd1f8fa29166c971083
Sarah Theoulle <sarah.theoulle@etu.umontpellier.fr>
2024-10-13 21:43:51 +0200

    DO purple

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,7 @@
 /**
  * Components
  */
-import { ButtonPrimary, ButtonOutline } from "./Button";
+// import { ButtonPrimary, ButtonOutline } from "./Button";
 
 
 const Hero = () => {
@@ -40,10 +40,10 @@ const Hero = () => {
           </div>
 
           <h2 className="headline-1 max-w-[15ch] sm:max-w-[20ch] lg:max-w-[15ch] mt-5 mb-8 lg:mb-10">
-            Building Scalable Modern Websites for the Future
+            Exploring the world of DevOps and Cloud
           </h2>
 
-          <div className="flex items-center gap-3">
+          {/* <div className="flex items-center gap-3">
             <ButtonPrimary
               label="Download CV"
               icon="download"
@@ -54,7 +54,7 @@ const Hero = () => {
               label="Scroll down"
               icon="arrow_downward"
             />
-          </div>
+          </div> */}
         </div>
 
         <div className="hidden lg:block">
