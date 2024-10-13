666db084f848ce420844b71ac622c14000797a91
Sarah Theoulle <sarah.theoulle@etu.umontpellier.fr>
2024-10-13 18:31:12 +0200

    mid app commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+
+import tailwindScrollbar from  'tailwind-scrollbar';
+
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'sans': ['Inter', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [tailwindScrollbar],
+}
+
