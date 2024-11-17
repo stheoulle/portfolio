4332c05f6f4557d15603767a87e9e0b74c4a5334
Sarah <sarah.theoulle@etu.umontpellier.fr>
2024-11-17 17:37:04 +0100

    feat(photo) : update pictures description

--- src/components/Photography.jsx
+++ src/components/Photography.jsx
@@ -3,90 +3,76 @@ const works = [
   {
     imgSrc: '/images/photography/descente.JPG', // Replace with relevant image path
     title: 'Descente de VTT',
-    tags: ['2021', 'Rust', 'Development'],
-    projectLink: 'https://github.com/dev-sys-do/sealci'
+    tags: ['2022']
   },
   {
     imgSrc: '/images/photography/fire.JPG', // Replace with relevant image path
     title: 'Faire feu de tout bois',
-    tags: ['2022', 'TailwindCSS', 'AdonisJS'],
-    projectLink: 'https://beep.ovh'
+    tags: ['2023']
   },
   
   {
     imgSrc: '/images/photography/fireworks.JPG', // Replace with relevant image path
     title: '14 juillet à Montpellier',
-    tags: ['2023', 'Gephi', 'Data'],
-    projectLink: 'https://github.com/stheoulle/twitch-data-visualization'
+    tags: ['2024'],
   },
   {
     imgSrc: '/images/photography/fise.JPG', // Replace with relevant image path
     title: 'FISE',
-    tags: ['2024', 'Conference', 'DevOps'],
-    projectLink: 'https://polycloud.fr/'
+    tags: ['2024'],
   },
   {
     imgSrc: '/images/photography/flower.JPG', // Replace with relevant image path
     title: 'Fleur',
-    tags: ['2024', 'CLI', 'Backups'],
-    projectLink: 'https://github.com/somoslosdorados/KDNA'
+    tags: ['2024'],
   },
   {
     imgSrc: '/images/photography/saintebarbe1.JPG', // Replace with relevant image path
     title: 'Festivité de la Sainte-Barbe',
-    tags: ['2023', 'Gephi', 'Data'],
-    projectLink: 'https://github.com/stheoulle/twitch-data-visualization'
+    tags: ['2022'],
   },
   
   {
     imgSrc: '/images/photography/omaha.JPG', // Replace with relevant image path
     title: "Plage d'Omaha",
-    tags: ['2022', 'TailwindCSS', 'AdonisJS'],
-    projectLink: 'https://beep.ovh'
+    tags: ['2022'],
   },  
   
   {
     imgSrc: '/images/photography/saintebarbe2.JPG', // Replace with relevant image path
     title: 'Festivité de la Sainte-Barbe',
-    tags: ['2024', 'Conference', 'DevOps'],
-    projectLink: 'https://polycloud.fr/'
+    tags: ['2022'],
   },
   {
     imgSrc: '/images/photography/saintetienne.JPG', // Replace with relevant image path
     title: 'Saint-Etienne',
-    tags: ['2024', 'CLI', 'Backups'],
-    projectLink: 'https://github.com/somoslosdorados/KDNA'
+    tags: ['2022'],
   },
   {
     imgSrc: '/images/photography/salagou.JPG', // Replace with relevant image path
     title: 'Lac du Salagou',
-    tags: ['2021', 'Rust', 'Development'],
-    projectLink: 'https://github.com/dev-sys-do/sealci'
+    tags: ['2024'],
   },
   {
     imgSrc: '/images/photography/heart.JPG', // Replace with relevant image path
     title: 'Coeur',
-    tags: ['2021', 'Rust', 'Development'],
-    projectLink: 'https://github.com/dev-sys-do/sealci'
+    tags: ['2024'],
   },
   {
     imgSrc: '/images/photography/stmichel.JPG', // Replace with relevant image path
     title: 'Mont Saint-Michel',
-    tags: ['2022', 'TailwindCSS', 'AdonisJS'],
-    projectLink: 'https://beep.ovh'
+    tags: ['2022'],
   },
   
   {
     imgSrc: '/images/photography/vtt.JPG', // Replace with relevant image path
     title: 'Cross-country olympique',
-    tags: ['2023', 'Gephi', 'Data'],
-    projectLink: 'https://github.com/stheoulle/twitch-data-visualization'
+    tags: ['2022'],
   },
   {
     imgSrc: '/images/photography/olympe.JPG', // Replace with relevant image path
     title: 'Jeux olympiques',
-    tags: ['2024', 'Conference', 'DevOps'],
-    projectLink: 'https://polycloud.fr/'
+    tags: ['2024'],
   }
 ];
 
